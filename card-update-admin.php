7569d43e9b2655ad98b3e5c5ccc5741e2d39a8f6
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-26 13:12:49 -0600

    card update

--- card-update-admin.php
+++ card-update-admin.php
@@ -29,36 +29,33 @@ echo <<<_END
     Points: $row[points]
 </pre>
 
-<form method='post' action='card-update-admin.php'>
+<form method='post' action='card-add-admin.php'>
 <pre>
     Card Name: <input type='text' name='cardName'>
     Card Type: <input type='text' name='cardType'>
     Card Value: <input type='decimal' name='cardValue'>
     Points: <input type='decimal' name='points'>
-    <input type='submit' value='Update Record'>
+    <input type='submit' value='Add Record'>
 </pre>
 </form>
 
 _END;
 }
 
-$conn = new mysqli ($hn, $un, $pw, $db);
-if($conn->connect_error) die($conn->connect_error);
-
 if(isset($_POST['cardName'])) {
-//    $cardId = $_POST['cardId'];
-    $cardName = $_POST['cardName'];
-    $cardType = $_POST['cardType'];
-    $cardValue = $_POST['cardValue'];
-    $points = $_POST['points'];
-
-    $query = "insert into giftcard (cardName, cardType, cardValue, points) values ('$cardName', '$cardType', $cardValue, $points)";
-
-    $conn->query($query);
-    if(!result) die ($conn->error);
-    header("Location: card-add-admin.php");
-}
-
-$conn->close();
+    //    $cardId = $_POST['cardId'];
+        $cardName = $_POST['cardName'];
+        $cardType = $_POST['cardType'];
+        $cardValue = $_POST['cardValue'];
+        $points = $_POST['points'];
+    
+        $query = "insert into giftcard (cardName, cardType, cardValue, points) values ('$cardName', '$cardType', $cardValue, $points)";
+    
+        $conn->query($query);
+        if(!result) die ($conn->error);
+        header("Location: card-add-admin.php");
+    }
+    
+    $conn->close();
 
 ?>
