34110abf8764a0929715ca35000178c5f7667758
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-26 13:09:21 -0600

    card update

--- card-update-admin.php
+++ card-update-admin.php
@@ -9,92 +9,54 @@ if($conn->connect_error) die($conn->connect_error);
 
 if (isset($_GET['cardId'])) {
 
-$isbn = $_GET['cardId'];
+$cardId = $_GET['cardId'];
 
-$query = "select * from giftcard where cardId=$cardid";
+$query = "select * from giftcard where cardId=$cardId";
 
 $result = $conn->query($query);
 if(!$result) die ($conn->error);
 
 $rows = $result->num_rows;
 
-for ($j=0; $j<$rows; ++$j) {
-//    $result->data_seek($j);
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    $cardname = $row['cardName'];
-    $checked='';
-    if($available==1) $checked='checked';
-
-    $category = $row['category'];
-    $A=$B=$C='';
-    if($category=='Classic Fiction') $A = 'selected';
-    if($category=='Non-Fiction') $B = 'selected';
-    if($category=='Play') $C = 'selected';
-
-    $genre = $row['genre'];
-    $D=$E=$F='';
-    if($genre=='action') $D = 'checked';
-    if($genre=='classics') $E = 'checked';
-    if($genre=='memoir') $F = 'checked';
-
-    $imagepath = $row['imagepath'];
+$row = $result->fetch_array(MYSQLI_ASSOC);
 
 echo <<<_END
 
-    <form action='updaterecord.php' method='post'>
-
 <pre>
-
-    <img src='$imagepath' width='200' height='300'>
-
-    Author: <input type='text' name='author' value='$row[author]'>
-    Title: <input type='text' name='title' value='$row[title]'>
-    Year: <input type='text' name='year' value='$row[year]'>
-    Available: Yes <input type='checkbox' name='available' $checked>
-    ISBN: $row[isbn]
-    Image Path: <input type='text' name='imagepath' value='$imagepath'>
-
-    Category:
-    <select name='category' id='category'>
-        <option value='Classic Fiction' $A>Classic Fiction</option>
-        <option value='Non-Fiction' $B>Non-Fiction</option>
-        <option value='Play' $C>Play</option>
-    </select>
-
-    <input type="radio" id="action" name="genre" value="action" $D>Action
-    <input type="radio" id="action" name="genre" value="classics" $E>Classics
-    <input type="radio" id="action" name="genre" value="memoir" $F>Memoir
-
+    Card Name: $row[cardName]
+    Card Type: $row[cardType]
+    Card Value: $row[cardValue]
+    Points: $row[points]
 </pre>
 
-        <input type='hidden' name='update' value='yes'>
-        <input type='hidden' name='isbn' value='$row[isbn]'>
-        <input type='submit' value='UPDATE RECORD'>
-    </form>
+<form method='post' action='card-update-admin.php'>
+<pre>
+    Card Name: <input type='text' name='cardName'>
+    Card Type: <input type='text' name='cardType'>
+    Card Value: <input type='decimal' name='cardValue'>
+    Points: <input type='decimal' name='points'>
+    <input type='submit' value='Update Record'>
+</pre>
+</form>
 
 _END;
 }
-}
 
-if (isset($_POST['update'])) {
-    $isbn = $_POST['isbn'];
-    $author = $_POST['author'];
-    $title = $_POST['title'];
-    $category = $_POST['category'];
-    $year = $_POST['year'];
+$conn = new mysqli ($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
 
-    $available=0;
-    if(isset($_POST['available'])) $available=1;
+if(isset($_POST['cardName'])) {
+//    $cardId = $_POST['cardId'];
+    $cardName = $_POST['cardName'];
+    $cardType = $_POST['cardType'];
+    $cardValue = $_POST['cardValue'];
+    $points = $_POST['points'];
 
-    $genre = $_POST['genre'];
-    $imagepath = $_POST['imagepath'];
+    $query = "insert into giftcard (cardName, cardType, cardValue, points) values ('$cardName', '$cardType', $cardValue, $points)";
 
-    $query = "UPDATE classics2 set author='$author', title='$title', category='$category', year='$year', available=$available, genre='$genre', imagepath='$imagepath' where isbn=$isbn";
-
-    $result = $conn->query($query);
-    if(!$result) die ($conn->error);
-
-    header("Location: card-list-admin.php");
+    $conn->query($query);
+    if(!result) die ($conn->error);
+    header("Location: card-add-admin.php");
 }
 
 $conn->close();
