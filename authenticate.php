95daf18609ddd3886cc9ad231231b4117fa135ea
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-26 00:57:35 -0600

    add authentication file

---
+++ authenticate.php
@@ -0,0 +1,49 @@
+include
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+// Get values from login screen
+    $tmp_username = mysql_entities_fix_string($conn, $_POST['username']);
+    $tmp_password = mysql_entities_fix_string($conn, $_POST['password']);
+
+    // get password from DB w/ SQL
+    $query = "select password from users where username = '$tmp_username'";
+
+    $result = $conn->query($query);
+    if(!$result) die($conn->error);
+
+    $rows = $result->num_rows;
+    $passwordFromDB="";
+    for($j=0; $j<$rows; $j++) {
+        $result->data_seek($j);
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        $passwordFromDB = $row['password'];
+    }
+
+    // compare passwords
+    if(password_verify($tmp_password, $passwordFromDB)) {
+        echo "successful login<br>";
+        session_start();
+        $_SESSION['username'] = $tmp_username;
+        echo "<a href='continue.php'> Continue </a>";
+    } else {
+        echo "login error<br>";
+    }
+
+}
+
+$conn->close();
+
+// sanitization functions
+function mysql_entities_fix_string($conn, $string) {
+    return htmlentities(mysql_fix_string($conn, $string));
+}
+
+function mysql_fix_string($conn, $string) {
+    $string = stripslashes($string);
+    return $conn->real_escape_string($string);
+}
+
+?>
