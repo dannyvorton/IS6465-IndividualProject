b2f224aab0fb26d3c53416f1c952f6232628fcc8
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-26 23:48:28 -0600

    update login

--- login.php
+++ login.php
@@ -4,36 +4,79 @@ include 'header.php';
 include 'database.php';
 
 echo <<<_END
-<main>
-<div class="login">
-            <h1>Login to Air Asia</h1>
-            <form method="post" action="authenticate.php">
-                <p>
-                    <input type="text" name="userName" value="" placeholder="Username or Email">
-                </p>
-                <p>
-                    <input type="password" name="password" value="" placeholder="Password">
-                </p>
-                <p class="remember_me">
-                    <label>
-                        <input type="checkbox" name="remember_me" id="remember_me">
-                        Remember me on this computer
-                    </label>
-                </p>
-                <p class="submit">
-                    <input type="submit" name="commit" value="login">
-                </p>
-            </form>
-        </div>
-        <div class="login_help">
-            <p>
-                New User?
-                <a href="register.php">Click Here to Register</a>
-            </p>
-        </div>
-    </body>
-</main>
-    </html>
+<body>
+<form method='post' action='login.php'>
+    Username: <input type='text' name='username'><br>
+    Password: <input type='password' name='password'>
+    <input type='submit' value='Login'>
+    <br>
+    New User?
+        <a href="register.php">Click Here to Register</a>
+</form>
+</body>
+</html>
 _END;
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
+
+if (isset($_POST['userName']) && isset($_POST['password'])) {
+	
+	//Get values from login screen
+	$tmp_username = mysql_entities_fix_string($conn, $_POST['userName']);
+	$tmp_password = mysql_entities_fix_string($conn, $_POST['password']);
+	
+	//get password from DB w/ SQL
+	$query = "select password from users where userName = '$tmp_username'";
+	
+	$result = $conn->query($query); 
+	if(!$result) die($conn->error);
+	
+	$rows = $result->num_rows;
+	$passwordFromDB="";
+	for($j=0; $j<$rows; $j++)
+	{
+		$result->data_seek($j); 
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		$passwordFromDB = $row['password'];
+	
+	}
+	
+	//Compare passwords
+	if(password_verify($tmp_password,$passwordFromDB))
+	{
+		echo "successful login<br>";
+
+		$user = new User($user);
+
+		session_start();
+		$_SESSION['user'] = $user;
+		
+//		echo "<a href='continue.php'> Continue </a>";
+
+		header("Location: card-list-admin.php");
+
+	}
+	else
+	{
+		echo "login error<br>";
+	}	
+	
+}
+
+$conn->close();
+
+
+//sanitization functions
+function mysql_entities_fix_string($conn, $string){
+	return htmlentities(mysql_fix_string($conn, $string));	
+}
+
+function mysql_fix_string($conn, $string){
+	$string = stripslashes($string);
+	return $conn->real_escape_string($string);
+}
+
+
 
 ?>
