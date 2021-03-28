9cac960dba84be552e39107245eab6b9f2975747
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2021-03-27 20:48:41 -0600

    update session

--- session.php
+++ session.php
@@ -2,28 +2,41 @@
 
 require_once 'user.php';
 
+$page_roles = array('admin','user');
+
 session_start();
 
-if(!isset($_SESSION['user'])) {
-    header("Location: login.php");
-    exit();
-} else {
-    $user = $_SESSION['user'];
+if($_SESSION['person']) {
+    $user = $_SESSION['person'];
     $username = $user->username;
     $user_roles = $user->getRoles();
 
-    $found = 0;
+    $found=0;
     foreach ($user_roles as $urole) {
         foreach ($page_roles as $prole) {
             if ($urole==$prole) {
-                $found=1;  
+                $found=1;
             }
         }
     }
 
     if (!$found) {
-        header("Location: unathorized.php");
+        header("Location: unauthorized.php");
+    } else {
+        echo "Welcome back $username<br>";
     }
+
+//    destroy_session_and_data();
+
+//    echo "Welcome back $username <br>";
+} else {
+    echo "Username is not in the session<br>";
+}
+
+function destroy_session_and_data() {
+    $_SESSION = array();
+    setcookie(session_name(), '', time() - 2592000, '/');
+    session_destroy();
 }
 
 ?>
